45c7a901895619585b5cce93690510c57d611f83
Roman Peters <romanpeters1993@gmail.com>
2017-12-04 14:35:29 +0100

    ["arguments"] toegevoegd aan msg

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import telepot
 import time
 import commands
+import sys
 
-commands = {"/start": commands.start}
+enabled = {"/start": commands.start}
 
 
 def on_msg(msg):
-    command = msg["text"]
+    text = msg["text"].split()
 
-    if command in commands.keys():
-        commands[command](msg, bot)
+    msg["command"] = text[0]
+
+    if len(text) > 1:
+        msg["args"] = text[1:]
+
+    if msg["command"] in enabled.keys():
+        commands[msg["command"]](msg, bot)
 
 
 
 if __name__ == '__main__':
-    bot = telepot.Bot("plaats hier je Telegram API key")
+    bot = telepot.Bot(sys.argv[1])
 
     print('Online...')
     bot.message_loop({'chat': on_msg,
