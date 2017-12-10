762baa15a536d9b9403f2251f997710c3cc6ca14
Roman Peters <romanpeters1993@gmail.com>
2017-12-10 17:01:58 +0100

    Heel veel comments toegevoegd

--- commands.py
+++ commands.py
@@ -1,2 +1,13 @@
+# Voeg hier je eigen commands toe
+
 def start(msg, bot):
-    bot.sendMessage(msg["chat"]["id"], "Hello world!")
+    # het msg-object bevat alle informatie van het bericht
+    # inclusief het command en args die wij hebben toegevoegd
+    print(f"command:   {msg['command']}") # f-strings zijn handig voor string interpolatie
+    print(f"arguments: {msg.get('args')}")
+
+    reply_text = f"Hello {msg['from']['first_name']}!"
+
+    # berichten terugsturen doe je naar het unieke ID van het chatgesprek
+    # gelukkig is dat ID meegegeven aan het oorspronkelijke bericht in msg["chat"]["id"]
+    bot.sendMessage(msg["chat"]["id"], reply_text)
--- main.py
+++ main.py
@@ -1,30 +1,45 @@
-import telepot
 import time
-import commands
 import sys
+import telepot   # Python wrapper voor de Telegram API
+import commands  # importeer je commands uit commands.py
 
+# text berichten linken aan commands
 enabled = {"/start": commands.start}
 
 
+
 def on_msg(msg):
-    text = msg["text"].split()
+    if msg.get("text"):  # als het een tekstbericht is
 
-    msg["command"] = text[0]
+        text = msg["text"].split()  # lijst met alle woorden uit het bericht
 
-    if len(text) > 1:
-        msg["args"] = text[1:]
+        msg["command"] = text[0]  # bewaar het eerste woord uit de lijst in msg["command"]
 
-    if msg["command"] in enabled.keys():
-        enabled[msg["command"]](msg, bot)
+        if len(text) > 1:  # als het bericht uit meerdere woorden bestaat
+            msg["args"] = text[1:]  # dan bewaar je de rest in msg["args"]
+
+        if msg["command"] in enabled.keys():  # als het command in onze lijst met commands staat
+            enabled[msg["command"]](msg, bot)  # dan voer je 'm uit, met als parameters het msg- en bot-object
+    else:
+        # je kunt eventueel ook nog wat doen met berichten die geen tekst hebben
+        # zoals locaties, of afbeeldingen.
+        pass  # doe dat dan hier
 
 
+if __name__ == '__main__':  # als het bestand als script wordt uitgevoerd
+    bot = telepot.Bot(sys.argv[1])  # pak het eerste argument van de command line voor het bot-object
+    # als het je niet uitmaakt dat hackers je API key zouden kunnen stelen
+    # kun je ipv sys.argv[1] ook rechtstreeks je API key hier in een string meegeven aan de functie
 
-if __name__ == '__main__':
-    bot = telepot.Bot(sys.argv[1])
 
-    print('Online...')
+    print('Online...')  # Laat even zien dat alles goed is geldadn
+
+
+    # de message_loop functie van het bot-object zorgt dat berichten naar onze functie worden doorgestuurd
     bot.message_loop({'chat': on_msg,
                       })
 
+    # het script moet draaiende blijven om de bot online te houden
+    # dus hier een infinite loop om 'm bezig te houden
     while 1:
         time.sleep(10)
